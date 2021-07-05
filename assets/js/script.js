a6af008e2eb65385c6c0d9b5778b95503c706873
lverghese <loganverghese@gmail.com>
2021-07-04 22:55:37 -0400

    submit and prevent default

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,6 @@ var createTaskHandler = function () {
     listItemEl.className = "task-item";
     listItemEl.textContent = ("This is a new task.");
     tasksToDoEl.appendChild(listItemEl);
-
-    console.log(event);
-}
+};
 
 formEl.addEventListener("submit", createTaskHandler);
